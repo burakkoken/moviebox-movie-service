dbcfe869f7e2ab8b0d4f53b8d5bed4ecca078a1f
Burak Koken <burakkokenn@gmail.com>
2020-01-03 00:04:34 +0200

    Added the movie and genre models, also added the datasource properties for postgresql

---
+++ src/main/java/org/codnect/moviebox/model/Genre.java
@@ -0,0 +1,69 @@
+package org.codnect.moviebox.model;
+
+import org.hibernate.annotations.NaturalId;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+/**
+ * Created by Burak Köken on 2.1.2020.
+ */
+@Entity
+@Table(name = "Genres")
+public class Genre {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NaturalId
+    @Column(name = "genre_name")
+    private String name;
+
+    @ManyToMany(mappedBy = "genres", fetch = FetchType.LAZY)
+    private Set<Movie> movies = new HashSet<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Movie> getMovies() {
+        return movies;
+    }
+
+    public void setMovies(Set<Movie> movies) {
+        this.movies = movies;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(this == obj) {
+            return true;
+        }
+        if(obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Genre genre = (Genre) obj;
+        return Objects.equals(name, genre.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+}
---
+++ src/main/java/org/codnect/moviebox/model/Movie.java
@@ -0,0 +1,122 @@
+package org.codnect.moviebox.model;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+/**
+ * Created by Burak Köken on 2.1.2020.
+ */
+@Entity
+@Table(name = "Movies")
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "movie_title")
+    private String title;
+
+    @Column(name = "movie_runtime")
+    private Integer runtime;
+
+    @Column(name = "movie_imdb")
+    private Double imdb;
+
+    @Column(name = "movie_release_date")
+    @Temporal(TemporalType.DATE)
+    private Date releaseDate;
+
+    @ManyToMany(
+            fetch = FetchType.LAZY,
+            cascade = {CascadeType.PERSIST, CascadeType.MERGE}
+    )
+    @JoinTable(
+            name = "Movie_Genre",
+            joinColumns = { @JoinColumn(name = "movie_id") },
+            inverseJoinColumns = { @JoinColumn(name = "genre_id") }
+    )
+    private Set<Genre> genres = new HashSet<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getRuntime() {
+        return runtime;
+    }
+
+    public void setRuntime(Integer runtime) {
+        this.runtime = runtime;
+    }
+
+    public Double getImdb() {
+        return imdb;
+    }
+
+    public void setImdb(Double imdb) {
+        this.imdb = imdb;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public Set<Genre> getGenres() {
+        return genres;
+    }
+
+    public void setGenres(Set<Genre> genres) {
+        this.genres = genres;
+    }
+
+    public void addGenre(Genre genre) {
+        genres.add(genre);
+        genre.getMovies().add(this);
+    }
+
+    public void removeGenre(Genre genre) {
+        genres.remove(genre);
+        genre.getMovies().remove(this);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(this == obj) {
+            return true;
+        }
+        if(obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Movie movie = (Movie) obj;
+        if(movie.id == null || id == null) {
+            return false;
+        }
+        return Objects.equals(id, movie.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+}
